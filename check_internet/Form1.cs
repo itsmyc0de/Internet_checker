6cc09326f6f507ba2892e6f12e826f84696ee7f9
Sudheesh S <itsmyc0de@users.noreply.github.com>
2017-09-20 15:20:58 +0530

    Add files via upload

---
+++ check_internet/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+namespace check_internet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int con = check_connection();
+            if (con == 0)
+            {
+                MessageBox.Show("Check The Internet Connection :(");
+            }
+            else
+            {
+                MessageBox.Show("You have the perfect inernet Connection !");
+            }
+            
+        }
+
+
+        public int check_connection(){
+            int val = 0;
+        bool conn = NetworkInterface.GetIsNetworkAvailable();
+            if (conn == true)
+            {
+                val = 1;
+            }
+            else
+            {
+                val = 0;
+            }
+
+            return val;
+        }
+
+    }
+}
